568006950915075c7748fdd924a581c70d4b2393
Vignesh R <vignesh.r@metaweb.net.au>
2026-07-16 21:51:24 +0530

    app

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,8 +14,6 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias(['role' => CheckRole::class]);
-
-        $middleware->statefulApi();
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
