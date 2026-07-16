9da00ea7c19060363a5ff89dd32c290062608964
Vignesh R <vignesh.r@metaweb.net.au>
2026-07-16 20:16:39 +0530

    db

--- config/database.php
+++ config/database.php
@@ -97,9 +97,7 @@ return [
             'prefix_indexes' => true,
             'search_path' => 'public',
             'sslmode' => env('DB_SSLMODE', 'prefer'),
-            'options' => array_filter([
-                PDO::PGSQL_ATTR_OPTIONS => env('DB_PGSQL_OPTIONS'),
-            ]),
+            'options' => env('DB_PGSQL_OPTIONS'),
         ],
 
         'sqlsrv' => [
