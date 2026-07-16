ada2332cd13fd56b83d6e415b8eeaf155b5a73e2
Vignesh R <vignesh.r@metaweb.net.au>
2026-07-16 21:21:39 +0530

    app service chages

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,21 +2,30 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Connection;
+use Illuminate\Database\Connectors\PostgresConnector;
 use Illuminate\Support\ServiceProvider;
+use PDO;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
-        //
+        if (env('DB_PGSQL_OPTIONS')) {
+            $this->app->bind('db.connector.pgsql', function () {
+                return new class extends PostgresConnector {
+                    public function connect(array $config): PDO
+                    {
+                        $dsn = $this->getDsn($config);
+                        $dsn .= ';options=' . env('DB_PGSQL_OPTIONS');
+                        $options = $this->getOptions($config);
+                        return $this->createConnection($dsn, $config, $options);
+                    }
+                };
+            });
+        }
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
         //
