61ce9b124bb2954d1957b23f96a556ebb3e9ff10
Vignesh R <vignesh.r@metaweb.net.au>
2026-07-19 15:11:17 +0530

    app

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -10,8 +10,10 @@ const queryClient = new QueryClient({
     defaultOptions: {
         queries: {
             retry: 1,
-            staleTime: 0,
-            gcTime: 5 * 60 * 1000,
+            staleTime: 5 * 60 * 1000,
+            gcTime: 30 * 60 * 1000,
+            refetchOnMount: false,
+            refetchOnReconnect: false,
             refetchOnWindowFocus: false,
         },
     },
